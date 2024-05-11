b19cde4484432c2fff48b798db79937526aee5f1
Грецкий Дмитрий Петрович <82818467+thereedlegend@users.noreply.github.com>
2024-05-11 15:13:37 +0300

    First steps

---
+++ FillingStation 1.10/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Система расчёта стоимости топлива:");
+
+        int fuelType = 92;
+        int amount = 420;
+
+        double fuel92price = 60.2;
+        double fuel95price = 67.33;
+
+        double fuelPrice = 0;
+        if (fuelType == 92) {
+            fuelPrice = fuel92price;
+        }
+        else if (fuelType == 95) {
+            fuelPrice = fuel95price;
+        } else {
+            System.out.println("Ошибка: указан неверный тип топлива");
+        }
+        if (amount < 1) {
+            System.out.println("Указано слишком малое количество топлива");
+            amount = 0;
+        }
+
+        System.out.println("Цена выбранного топлива: " + fuelPrice + " руб.");
+
+        double totalPrice = fuelPrice * amount;
+        System.out.println("Общая стоимость заправки: " + totalPrice + " руб.");
+    }
+}
